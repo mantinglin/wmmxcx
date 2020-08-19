e869841386b2e56d08ee34b3d4f240e3d79c28cb
mantinglin <mantinglin23@gmail.com>
2020-08-19 14:32:02 -0700

    Split components into separate files (#6)

---
+++ components/ChoresScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout, Text } from '@ui-kitten/components';
+
+export default function ChoresScreen() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>家务分配</Text>
+    </Layout>
+  );
+}
